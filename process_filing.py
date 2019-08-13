21047332265af0fb25731648b3378328aad673c4
rshorey <rshorey@gmail.com>
2019-08-13 14:56:25 -0400

    was trying to process blank itemization, fixed

--- process_filing.py
+++ process_filing.py
@@ -101,8 +101,8 @@ def itemization_iterator(path, filing_id, fec_version_number, fec_file=False):
                         filing_id = path.strip('/').split('/')[-1].split('.')[0]
                     except:
                         filing_id = None
-            itemization['filing_id'] = filing_id
-            yield itemization
+                itemization['filing_id'] = filing_id
+                yield itemization
 
 def process_summary_row(summary_row, fec_version_number):
     #processes the second row of the filing, which is the form summary/topline row
